b8d6a8a97d5282e239d684549a497a65e3447996
TomYumCoder <linoger67@gmail.com>
2023-01-02 23:03:54 +0300

    Fine version 1. Everything works well

--- Telebot-subchecker.py
+++ Telebot-subchecker.py
@@ -3,7 +3,6 @@ import telebot
 from telebot.apihelper import ApiTelegramException
 
 bot = telebot.TeleBot(os.getenv("TOKEN"))
-photo = open("XOWMtjt.png", 'rb')
 
 CHAT_ID = -1001741609743
 
@@ -11,29 +10,39 @@ button_pikatchu = telebot.types.InlineKeyboardButton('Send a picture', callback_
 keyboard = telebot.types.InlineKeyboardMarkup()
 keyboard.add(button_pikatchu)
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     mess = f"Hello,<b>{message.from_user.first_name} <u>{message.from_user.last_name}!</u> </b>\n use this " \
            f"bot to check if you subscribed to speciffic telegram channel or not by /help "
+    print(message.from_user.id)
     bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=keyboard)
 
 
 def is_subscribed(CHAT_ID, user_id):
+    print(bot.get_chat_member(CHAT_ID, user_id).status)
     try:
+        # print(bot.get_chat_member(CHAT_ID, user_id))
         bot.get_chat_member(CHAT_ID, user_id)
         return True
     except ApiTelegramException as e:
         if e.result_json['description'] == 'Bad Request: user not found':
             return False
 
-@bot.callback_query_handler(func=lambda call:True)
+        return False
+
+@bot.callback_query_handler(func=lambda call: True)
 def callback(call):
+    print(bot.get_chat_member(CHAT_ID, call.from_user.id))
     if call.data == 'button_pressed':
-        if not is_subscribed(CHAT_ID, call.message.from_user.id):
-            bot.send_message(call.message.chat.id, "please join @ai_drawing to continue",
-                             parse_mode='html')
+        if bot.get_chat_member(CHAT_ID, call.from_user.id).status == "left":
+            bot.send_message(call.message.chat.id, "please join @ai_drawing to continue", parse_mode='html')
+        elif not is_subscribed(CHAT_ID, call.from_user.id):
+            # print(USER_ID)
+            bot.send_message(call.message.chat.id, "please join @ai_drawing to continue", parse_mode='html')
         else:
-            bot.send_photo(call.message.chat.id,photo,"here you are" )
+            bot.send_photo(call.message.chat.id, open("XOWMtjt.png", 'rb'), "here you are")
+
 
 @bot.message_handler(commands=['help'])
 def help_command(message):
